bea2db798d4c2a00b1788f356a11219cdb2fe0f8
MarkMamrot <mark@monsterthreads.com.au>
2026-03-23 16:25:23 +1100

    Bypass strict linting/typechecking rules on cPanel builds

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors. Perfect for bypassing cPanel strictness.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
